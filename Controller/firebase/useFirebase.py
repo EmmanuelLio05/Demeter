0a18062fc4c2191f9ca6af47d6ece65a8945665f
EmmanuelLio05 <70165675+EmmanuelLio05@users.noreply.github.com>
2022-11-17 22:13:36 -0600

    [ADD] useFirebase

---
+++ Controller/firebase/useFirebase.py
@@ -0,0 +1,32 @@
+import os
+import time
+import sys
+sys.path.insert(0, '/libs')
+import ufirebase as firebase
+
+
+class useFirebase:
+    def __init__(self):
+        firebase.setURL("https://hermes-f3af7-default-rtdb.firebaseio.com/")
+    
+    def setTemperature(self,nTemperature):
+        firebase.put("telemetry/temperature", nTemperature, bg=0)
+
+    def setHumidity(self,nHumidity):
+        firebase.put("telemetry/temperature", nHumidity, bg=0)
+
+    def setBrightness(self,nBrightness):
+        firebase.put("telemetry/temperature", nBrightness, bg=0)
+
+    def getFan(self):
+        firebase.get.get("control/fan", "bFan", bg=0)
+        return firebase.bFan
+
+    def getWater(self):
+        firebase.get.get("control/fan", "bWater", bg=0)
+        return firebase.bWater
+
+    def getLight(self):
+        firebase.get("control/light", "bLight", bg=0)
+        return firebase.bLight
+        
--- Views/useOLED.py
+++ Views/useOLED.py
@@ -1,14 +1,17 @@
+import sys
+sys.path.insert(0, '/libs')
+
 import machine
 import ssd1306
 from time import sleep
 
-class oseOLED:
+class useOLED:
     def __init__(self):
         self.i2c = machine.SoftI2C(scl=machine.Pin(22), sda=machine.Pin(21))
         self.pin = machine.Pin(16, machine.Pin.OUT)
         self.pin.value(0) #Configura GPIO16 en bajo para resetear el OLED
         self.pin.value(1) #Mientras que el OLED esté ejecutándose, GPIO16 debe estar en 1
-        self.oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+        self.oled = ssd1306.SSD1306_I2C(128, 64, self.i2c)
         self.oled.fill(0)
         self.oled.show()
         self.aTemperature = [0x00, 0x00, 0x03, 0xc0, 0x02, 0x40, 0x02, 0x00, 0x02, 0x40, 0x02, 0x00, 0x02, 0x00, 0x02, 0x40,
@@ -17,6 +20,7 @@ class oseOLED:
 	                      0x30, 0x0c, 0x18, 0x18, 0x0c, 0x30, 0x07, 0xe0, 0x04, 0x20, 0x06, 0x60, 0x02, 0x40, 0x03, 0xc0]
         self.aLight = [0x07, 0xe0, 0x0f, 0xf0, 0x18, 0x18, 0x30, 0x0c, 0x30, 0x0c, 0x20, 0x04, 0x20, 0x04, 0x30, 0x0c, 
 	                   0x30, 0x0c, 0x18, 0x18, 0x0c, 0x30, 0x07, 0xe0, 0x04, 0x20, 0x06, 0x60, 0x02, 0x40, 0x03, 0xc0]
+        self.oled.blit(self.aLight,0,0)
 
     def display_parameter(self, parameter, value):
         self.oled.text(0, 0, value)
