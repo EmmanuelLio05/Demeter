7f07eaa4990c82bd0d7914a438bc118bf4485bd3
EmmanuelLio05 <70165675+EmmanuelLio05@users.noreply.github.com>
2022-11-27 23:53:44 -0600

    [UPDATE] useFirebase

--- Controller/firebase/useFirebase.py
+++ Controller/firebase/useFirebase.py
@@ -13,23 +13,28 @@ class useFirebase:
         firebase.put("telemetry/temperature", nTemperature, bg=0)
 
     def setHumidity(self,nHumidity):
-        firebase.put("telemetry/temperature", nHumidity, bg=0)
+        firebase.put("telemetry/humidity", nHumidity, bg=0)
 
     def setBrightness(self,nBrightness):
-        firebase.put("telemetry/temperature", nBrightness, bg=0)
-
-    def setWater(self,nWater):
-        firebase.put("control/water", nWater, bg=0)
+        firebase.put("telemetry/brightness", nBrightness, bg=0)
 
     def getFan(self):
         firebase.get("control/fan", "bFan", bg=0)
         return firebase.bFan
 
     def getWater(self):
-        firebase.get("control/fan", "bWater", bg=0)
+        firebase.get("control/Water", "bWater", bg=0)
         return firebase.bWater
 
     def getLight(self):
         firebase.get("control/light", "bLight", bg=0)
         return firebase.bLight
         
+    def setFan(self, bFan):
+        firebase.put("control/fan", bFan, bg=0)
+
+    def setWater(self, nWater):
+        firebase.put("control/water", nWater, bg=0)
+
+    def setLight(self, bLight):
+        firebase.put("control/light", bLight, bg=0)
